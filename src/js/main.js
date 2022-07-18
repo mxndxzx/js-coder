0cee422b8e19284e7f64d85f12e5b61f9487af4c
mxndxzx <mendozadevworks@gmail.com>
2022-07-18 20:05:13 -0300

    Challenge 4 - DOM only

--- src/js/main.js
+++ src/js/main.js
@@ -1,110 +1,103 @@
 // Variables
-let input;
-let opt;
-let addInput;
+
 
 // Arrays
-let market = ["huevos", "carne", "pollo", "galletitas", "harina"];
-let fruits = ["bananas", "paltas", "kiwis", "manzanas", "naranjas"];
-let library = ["lapiceras", "lápices", "cuadernos", "marcadores", "hojas"];
+const market = ["huevos", "galletitas", "harina"];
+const fruits = ["bananas", "paltas", "kiwis"];
+const library = ["lapiceras", "lápices", "cuadernos"];
+
 
 // Funciones
-let start = () => {
-  do {
-    input = parseInt(prompt
-      ("Las listas son: \n\n"
-      + "1. Supermercado:  " + market.join(", ") + "\n\n"
-      + "2. Frutas:  " + fruits.join(", ") + "\n\n"
-      + "3. Librería:  " + library.join(", ") + "\n\n"
-      + "Para agregar o quitar un elemento, seleccione una lista (1, 2, 3)"
-      ));
-
-      if (isNaN(input) || (input < 1) || (input > 3)) {
-        alert("Ingrese un número válido");
-      };}
-  while (isNaN(input) || (input < 1) || (input > 3));
-
-  switch (input) {
-    case 1 :
-      option(market);
-      break;
-    case 2 :
-      option(fruits);
-      break;
-    case 3 :
-      option(library);
-      break;
-    default:
-      alert("Opción incorrecta");
-      start();
-  };
+const capitalize = (word) => {
+  let newWord = word.charAt(0).toUpperCase() + word.slice(1);
+  return newWord;
 };
 
-let option = (arrName) => {
-  do {
-    opt = parseInt(prompt(
-      "La lista seleccionada contiene:  " + arrName.join(", ") + "\n\n"
-      + "1. Agregar elementos \n"
-      + "2. Quitar elementos"
-    ));
+const lists = () => {
+  const marketList = document.querySelector('.market ul');
+  market.forEach(item => {
+    marketList.innerHTML += `<li>- ${capitalize(item)}</li>`;
+  });
 
-    if ((opt < 1 ) || (opt > 2)) {
-      alert("Ingrese un número válido");
-    };}
-  while ((opt < 1) || (opt > 2));
+  const fruitsList= document.querySelector('.fruits ul');
+  fruits.forEach(item => {
+    fruitsList.innerHTML += `<li>- ${capitalize(item)}</li>`;
+  });
 
-  switch (opt) {
-    case 1 :
-      add(arrName);
-      break;
-    case 2 :
-      sus(arrName);
-      break;
-    default:
-      alert("Opción incorrecta");
-  };
+  const libraryList= document.querySelector('.library ul');
+  library.forEach(item => {
+    libraryList.innerHTML += `<li>- ${capitalize(item)}</li>`;
+  });
 };
 
-let add = (arrName) => {
-  addInput = prompt(
-    "Ingrese los elementos que desee agregar, separados por comas: "
-  );
+// Calls
+lists();
 
-  let replaceArr = addInput.replace(/\s+/g, "");
-  let splitArr = replaceArr.split(",");
-  splitArr.forEach(items);
-  function items(item) {
-    arrName.push(item);
-  };
+// let option = (arrName) => {
+//   do {
+//     opt = parseInt(prompt(
+//       "La lista seleccionada contiene:  " + arrName.join(", ") + "\n\n"
+//       + "1. Agregar elementos \n"
+//       + "2. Quitar elementos"
+//     ));
 
-  alert(
-    "Lista actualizada: \n"
-    + arrName.join(", "));
-  start();
-}
+//     if ((opt < 1 ) || (opt > 2)) {
+//       alert("Ingrese un número válido");
+//     };}
+//   while ((opt < 1) || (opt > 2));
 
-let sus = (arrName) => {
-  susInput = prompt(
-    "Ingrese los elementos que desee eliminar, separados por comas: "
-  );
+//   switch (opt) {
+//     case 1 :
+//       add(arrName);
+//       break;
+//     case 2 :
+//       sus(arrName);
+//       break;
+//     default:
+//       alert("Opción incorrecta");
+//   };
+// };
 
-  let replaceArr = susInput.replace(/\s+/g, "");
-  const splitArr = replaceArr.split(",");
-  splitArr.forEach(items);
-  function items(item) {
-    let index = arrName.indexOf(item);
-    if (index > -1) {
-      arrName.splice(index, 1);
-    } else {
-      alert("No se encontró un elemento. No se hicieron modificaciones");
-    }
-  };
+// let add = (arrName) => {
+//   addInput = prompt(
+//     "Ingrese los elementos que desee agregar, separados por comas: "
+//   );
 
-  alert(
-    "Lista actualizada: \n"
-    + arrName.join(", "));
-  start();
-}
+//   let replaceArr = addInput.replace(/\s+/g, "");
+//   let splitArr = replaceArr.split(",");
+//   splitArr.forEach(items);
+//   function items(item) {
+//     arrName.push(item);
+//   };
 
-// Call
-start();
+//   alert(
+//     "Lista actualizada: \n"
+//     + arrName.join(", "));
+//   start();
+// }
+
+// let sus = (arrName) => {
+//   susInput = prompt(
+//     "Ingrese los elementos que desee eliminar, separados por comas: "
+//   );
+
+//   let replaceArr = susInput.replace(/\s+/g, "");
+//   const splitArr = replaceArr.split(",");
+//   splitArr.forEach(items);
+//   function items(item) {
+//     let index = arrName.indexOf(item);
+//     if (index > -1) {
+//       arrName.splice(index, 1);
+//     } else {
+//       alert("No se encontró un elemento. No se hicieron modificaciones");
+//     }
+//   };
+
+//   alert(
+//     "Lista actualizada: \n"
+//     + arrName.join(", "));
+//   start();
+// }
+
+// // Call
+// start();
